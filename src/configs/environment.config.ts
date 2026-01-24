c66a1d76f708c2e7905ac49cfdd0367a5ed667f6
Sorokovskys <Sorokovskys@ukr.net>
2026-01-24 15:53:15 +0200

    environments

--- src/configs/environment.config.ts
+++ src/configs/environment.config.ts
@@ -1,5 +1,5 @@
 export const Environment = {
-  SERVER_URL: process.env["NG_APP_SERVER_URL"]
+  SERVER_URL: import.meta.env["NG_APP_SERVER_URL"]
 } as const;
 
 export type Environment = typeof Environment[keyof typeof Environment];
