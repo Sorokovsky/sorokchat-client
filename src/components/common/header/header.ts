b3052271e7fea1f8417841b07ec428a12e77013b
Sorokovskys <Sorokovskys@ukr.net>
2026-01-24 18:12:36 +0200

    refactor

--- src/components/common/header/header.ts
+++ src/components/common/header/header.ts
@@ -1,8 +1,4 @@
 import {Component} from '@angular/core';
-import {injectProfileQuery} from '@/injections/profile.query';
-import {CreateQueryResult} from '@tanstack/angular-query-experimental';
-import {User} from '@/contracts/user.contrcact';
-import {ProblemDetail} from '@/contracts/problem-detail.contract';
 
 @Component({
   selector: 'app-header',
@@ -11,5 +7,4 @@ import {ProblemDetail} from '@/contracts/problem-detail.contract';
   styleUrl: './header.sass',
 })
 export class Header {
-  protected readonly profileQuery: CreateQueryResult<User, ProblemDetail> = injectProfileQuery();
 }
