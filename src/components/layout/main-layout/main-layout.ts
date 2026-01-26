5d80270186c14176f4456a99f096b1d2e0a334f2
Sorokovsky <Sorokovskys@ukr.net>
2026-01-26 20:21:44 +0200

    login page

--- src/components/layout/main-layout/main-layout.ts
+++ src/components/layout/main-layout/main-layout.ts
@@ -1,12 +1,12 @@
 import {Component} from '@angular/core';
 import {HeaderComponent} from '@/components/common/header/header.component';
-import {LoginFormComponent} from '@/components/common/login-form/login-form.component';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-main-layout',
   imports: [
     HeaderComponent,
-    LoginFormComponent
+    RouterOutlet
   ],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.sass',
--- src/constants/app.routes.ts
+++ src/constants/app.routes.ts
@@ -1,3 +1,10 @@
 import {Routes} from '@angular/router';
+import {LoginPage} from '@/components/pages/login/login.page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    title: "Вхід",
+    path: "login",
+    component: LoginPage
+  }
+];
