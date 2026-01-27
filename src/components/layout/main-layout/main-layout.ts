c42ff9623a16dfdcd202734c04621360427c8999
Sorokovsky <Sorokovskys@ukr.net>
2026-01-27 17:52:41 +0200

    pages guard in layout

--- src/components/layout/main-layout/main-layout.ts
+++ src/components/layout/main-layout/main-layout.ts
@@ -1,6 +1,13 @@
-import {Component} from '@angular/core';
+import {Component, effect} from '@angular/core';
 import {HeaderComponent} from '@/components/common/header/header.component';
-import {RouterOutlet} from '@angular/router';
+import {Router, RouterOutlet} from '@angular/router';
+import {injectProfileQuery} from '@/injections/profile.query';
+import {CreateQueryResult} from '@tanstack/angular-query-experimental';
+import {User} from '@/contracts/user.contrcact';
+import {ProblemDetail} from '@/contracts/problem-detail.contract';
+import {ANONYMOUS_PAGES, LOGIN_PAGE, SECURED_PAGES} from '@/constants/pages.constants';
+import {Location} from '@angular/common';
+import {isContainsPage} from '@/utils/contains-page.util';
 
 @Component({
   selector: 'app-main-layout',
@@ -12,5 +19,20 @@ import {RouterOutlet} from '@angular/router';
   styleUrl: './main-layout.sass',
 })
 export class MainLayout {
+  private readonly profile: CreateQueryResult<User, ProblemDetail> = injectProfileQuery();
 
+  constructor(private readonly router: Router, private readonly location: Location) {
+    effect((): void => {
+      const currentPath: string = location.path().replace("/", "");
+      const isSecured: boolean = isContainsPage(SECURED_PAGES, currentPath);
+      const isAnonymousPage: boolean = isContainsPage(ANONYMOUS_PAGES, currentPath);
+      const isAuthenticated: boolean = this.profile.data() !== undefined;
+      if (isAuthenticated && isAnonymousPage) {
+        this.router.navigate(["/"])
+      }
+      if (!isAuthenticated && isSecured) {
+        this.router.navigate(["/", LOGIN_PAGE.path]);
+      }
+    });
+  }
 }
---
+++ src/utils/contains-page.util.ts
@@ -0,0 +1,5 @@
+import {Page} from '@/constants/pages.constants';
+
+export function isContainsPage(pages: Page[], path: string): boolean {
+  return pages.some((page: Page): boolean => page.path === path);
+}
