f28d0d982c052744d71262ad5af0b65145868e09
Sorokovsky <Sorokovskys@ukr.net>
2026-01-28 16:14:12 +0200

    page loader

--- src/components/layout/main-layout/main-layout.ts
+++ src/components/layout/main-layout/main-layout.ts
@@ -2,17 +2,25 @@ import {Component} from '@angular/core';
 import {HeaderComponent} from '@/components/common/header/header.component';
 import {RouterOutlet} from '@angular/router';
 import {injectAuthenticationGuard} from '@/injections/authentication.guard';
+import {CreateQueryResult} from '@tanstack/angular-query-experimental';
+import {User} from '@/contracts/user.contrcact';
+import {ProblemDetail} from '@/contracts/problem-detail.contract';
+import {injectProfileQuery} from '@/injections/profile.query';
+import {PageLoaderComponent} from '@/components/ui/page-loader/page-loader.component';
 
 @Component({
   selector: 'app-main-layout',
   imports: [
     HeaderComponent,
-    RouterOutlet
+    RouterOutlet,
+    PageLoaderComponent
   ],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.sass',
 })
 export class MainLayout {
+  protected readonly profile: CreateQueryResult<User, ProblemDetail> = injectProfileQuery();
+
   constructor() {
     injectAuthenticationGuard();
   }
