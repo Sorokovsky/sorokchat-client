845422fef575f2636251c40c7c3fc866de156620
Sorokovskys <Sorokovskys@ukr.net>
2026-01-29 15:21:08 +0200

    toggle avatar context

--- src/components/common/header-avatar/header-avatar.component.ts
+++ src/components/common/header-avatar/header-avatar.component.ts
@@ -1,4 +1,4 @@
-import {Component, input, InputSignal, signal, Signal} from '@angular/core';
+import {Component, input, InputSignal, signal, Signal, WritableSignal} from '@angular/core';
 import {CurrentUserAvatarComponent} from '@/components/common/current-user-avatar/current-user-avatar.component';
 import {ContextMenuComponent} from '@/components/ui/context-menu/context-menu.component';
 import {HEADER_PAGES, Page} from '@/constants/pages.constants';
@@ -15,7 +15,7 @@ import {injectGetPageByAccessRule} from '@/injections/inject-get-page-by.guard';
 })
 export class HeaderAvatarComponent {
   protected readonly menu: Signal<Page[]> = injectGetPageByAccessRule(HEADER_PAGES);
-  protected readonly isMenuOpen: Signal<boolean> = signal<boolean>(false);
+  protected readonly isMenuOpen: WritableSignal<boolean> = signal<boolean>(false);
   protected readonly positionStyles: InputSignal<string> = input<string>(
     "top: 60rem;" +
     "left: 0;" +
@@ -23,6 +23,15 @@ export class HeaderAvatarComponent {
     "z-index: 2;",
   );
 
-  protected hideSignal(): void {
+  protected hideContext(): void {
+    this.isMenuOpen.set(false);
+  }
+
+  protected showContext(): void {
+    this.isMenuOpen.set(true);
+  }
+
+  protected toggleContext(): void {
+    this.isMenuOpen.update((value: boolean): boolean => !value);
   }
 }
