d2447c81e2bb834d1a2d3b315a9da8651ccc52af
Sorokovsky <Sorokovskys@ukr.net>
2026-01-29 19:18:55 +0200

    avatar context auto close

--- src/components/common/header-avatar/header-avatar.component.ts
+++ src/components/common/header-avatar/header-avatar.component.ts
@@ -1,4 +1,4 @@
-import {Component, input, InputSignal, signal, Signal, WritableSignal} from '@angular/core';
+import {Component, input, InputSignal, OnDestroy, OnInit, signal, Signal, WritableSignal} from '@angular/core';
 import {CurrentUserAvatarComponent} from '@/components/common/current-user-avatar/current-user-avatar.component';
 import {ContextMenuComponent} from '@/components/ui/context-menu/context-menu.component';
 import {HEADER_PAGES, Page} from '@/constants/pages.constants';
@@ -13,7 +13,16 @@ import {injectGetPageByAccessRule} from '@/injections/inject-get-page-by.guard';
   templateUrl: './header-avatar.component.html',
   styleUrl: './header-avatar.component.sass',
 })
-export class HeaderAvatarComponent {
+export class HeaderAvatarComponent implements OnInit, OnDestroy {
+
+  public ngOnInit(): void {
+    document.addEventListener("click", this.handleClick.bind(this));
+  }
+
+  public ngOnDestroy(): void {
+    document.removeEventListener("click", this.handleClick.bind(this));
+  }
+
   protected readonly menu: Signal<Page[]> = injectGetPageByAccessRule(HEADER_PAGES);
   protected readonly isMenuOpen: WritableSignal<boolean> = signal<boolean>(false);
   protected readonly positionStyles: InputSignal<string> = input<string>(
@@ -33,5 +42,13 @@ export class HeaderAvatarComponent {
 
   protected toggleContext(): void {
     this.isMenuOpen.update((value: boolean): boolean => !value);
+  }
+
+  private handleClick(event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+    const isAvatar: boolean = target.closest(".avatar") !== null;
+    if (!isAvatar) {
+      this.hideContext();
+    }
   }
 }
