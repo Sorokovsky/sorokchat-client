b42fc66b0a79c406f9bf5322a8f712bc8461fee3
Sorokovsky <Sorokovskys@ukr.net>
2026-01-31 21:54:37 +0200

    feat: show chats sidebar if authenticated

--- src/components/layout/main-layout/main-layout.ts
+++ src/components/layout/main-layout/main-layout.ts
@@ -1,10 +1,11 @@
-import {Component} from '@angular/core';
+import {Component, Signal} from '@angular/core';
 import {HeaderComponent} from '@/components/common/header/header.component';
 import {RouterOutlet} from '@angular/router';
 import {injectAuthenticationGuard} from '@/injections/authentication.guard';
 import {injectProfileQuery, ProfileQuery} from '@/injections/profile.query';
 import {PageLoaderComponent} from '@/components/ui/page-loader/page-loader.component';
 import {ChatsSidebarComponent} from '@/components/common/chats-sidebar/chats-sidebar.component';
+import {injectIsAuthenticated} from '@/injections/is-authenticated.injection';
 
 @Component({
   selector: 'app-main-layout',
@@ -19,6 +20,7 @@ import {ChatsSidebarComponent} from '@/components/common/chats-sidebar/chats-sid
 })
 export class MainLayout {
   protected readonly profile: ProfileQuery = injectProfileQuery();
+  protected readonly isAuthenticated: Signal<boolean> = injectIsAuthenticated();
 
   constructor() {
     injectAuthenticationGuard();
