0f1de072c19f4a59dd110c830d6b23365e1cd55e
Sorokovsky <Sorokovskys@ukr.net>
2026-02-04 23:40:21 +0200

    feature: layout

--- src/components/layout/main-layout/main-layout.ts
+++ src/components/layout/main-layout/main-layout.ts
@@ -3,11 +3,13 @@ import {RouterOutlet} from '@angular/router';
 import {injectAuthenticationGuard} from '@/injections/authentication.guard';
 import {injectProfileQuery, type ProfileQuery} from '@/injections/profile.query';
 import {injectIsAuthenticated} from '@/injections/is-authenticated.injection';
+import {Sidebar} from '@/components/common/sidebar/sidebar';
 
 @Component({
   selector: 'app-main-layout',
   imports: [
     RouterOutlet,
+    Sidebar,
   ],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.sass',
