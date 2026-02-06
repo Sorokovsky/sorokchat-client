8f929b94f9cd96c9946062fe0f724ba6396cf95f
Sorokovsky <Sorokovskys@ukr.net>
2026-02-06 20:46:15 +0200

    refactor: sidebar-menu

--- src/components/common/sidebar-menu/sidebar-menu.ts
+++ src/components/common/sidebar-menu/sidebar-menu.ts
@@ -1,26 +1,21 @@
-import {Component, input, type InputSignal, type Signal} from '@angular/core';
+import {Component, input, type InputSignal} from '@angular/core';
 import {LucideAngularModule} from "lucide-angular";
-import {RouterLink} from '@angular/router';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 import {type Page} from '@/types';
 import {removeDynamicRoute} from '@/utils/replace-dynamic-route.util';
-import {injectCurrentPath} from '@/injections/current-path.injection';
 
 @Component({
   selector: 'app-sidebar-menu',
   imports: [
     LucideAngularModule,
-    RouterLink
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './sidebar-menu.html',
   styleUrl: './sidebar-menu.sass',
 })
 export class SidebarMenu {
   public readonly menu: InputSignal<Page[]> = input.required<Page[]>();
-  private readonly path: Signal<string> = injectCurrentPath();
-
-  public isActive(page: Page): boolean {
-    return this.path().includes(removeDynamicRoute(page.path));
-  }
 
   public getPath(page: Page): string {
     return removeDynamicRoute(page.path);
