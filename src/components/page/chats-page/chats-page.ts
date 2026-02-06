f0ec0a726b410072e8f3cdafaeec62a9a5e89c2b
Sorokovsky <Sorokovskys@ukr.net>
2026-02-06 17:00:03 +0200

    feature: getting chat

--- src/components/page/chats-page/chats-page.ts
+++ src/components/page/chats-page/chats-page.ts
@@ -1,6 +1,7 @@
-import {Component, type Signal} from '@angular/core';
+import {Component, computed, type Signal} from '@angular/core';
 import {type GetChatsByMe, injectGetChatsByMe} from '@/injections/get-chats-by-me.query';
 import {injectRouteParameter} from '@/injections/route-parameter.injection';
+import {type Chat} from '@/contracts/chat.contract';
 
 @Component({
   selector: 'app-chats-page',
@@ -11,4 +12,11 @@ import {injectRouteParameter} from '@/injections/route-parameter.injection';
 export class ChatsPage {
   protected readonly chatId: Signal<string | null> = injectRouteParameter("chatId");
   private readonly chatsQuery: GetChatsByMe = injectGetChatsByMe();
+  protected currentChat: Signal<Chat | null> = computed((): Chat | null => {
+    const id: string | null = this.chatId();
+    if (id === null) return null;
+    if (Number.isNaN(id)) return null;
+    const chat: Chat[] = this.chatsQuery.data() || [];
+    return chat.find((item: Chat): boolean => item.id === Number(id)) ?? null;
+  });
 }
