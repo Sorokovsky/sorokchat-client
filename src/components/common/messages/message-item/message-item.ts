c074139cd41dbe99634232712e75c53b8c724472
Sorokovsky <Sorokovskys@ukr.net>
2026-02-06 23:18:23 +0200

    feature: runtime update timestamp

--- src/components/common/messages/message-item/message-item.ts
+++ src/components/common/messages/message-item/message-item.ts
@@ -1,4 +1,14 @@
-import {Component, computed, input, type InputSignal, type Signal} from '@angular/core';
+import {
+  Component,
+  computed,
+  input,
+  type InputSignal,
+  type OnDestroy,
+  type OnInit,
+  signal,
+  type Signal,
+  type WritableSignal
+} from '@angular/core';
 import {Avatar} from '@/components/ui/avatar/avatar';
 import {getUserInitials} from '@/utils/get-user-initials.util';
 import {type ChatMessage} from '@/types/chst-message.type';
@@ -13,10 +23,27 @@ import {injectProfileQuery, type ProfileQuery} from '@/injections/authorization/
   templateUrl: './message-item.html',
   styleUrl: './message-item.sass',
 })
-export class MessageItem {
+export class MessageItem implements OnInit, OnDestroy {
   public readonly currentMessage: InputSignal<ChatMessage> = input.required<ChatMessage>();
   protected readonly profile: ProfileQuery = injectProfileQuery();
   protected readonly authorInitials: Signal<string> = computed((): string => getUserInitials(this.currentMessage().author));
-  protected readonly formattedDate: Signal<string> = computed((): string => formatDate(this.currentMessage().updatedAt));
+  private readonly tick: WritableSignal<Date> = signal<Date>(new Date());
+  protected readonly formattedDate: Signal<string> = computed((): string => {
+    this.tick();
+    return formatDate(this.currentMessage().updatedAt)
+  });
   protected readonly isMe: Signal<boolean> = computed((): boolean => this.currentMessage().author?.id === this.profile.data()?.id);
+  private interval: number | null = null;
+
+  public ngOnInit(): void {
+    this.interval = setInterval((): void => {
+      this.tick.set(new Date());
+    }, 1000);
+  }
+
+  public ngOnDestroy(): void {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
 }
