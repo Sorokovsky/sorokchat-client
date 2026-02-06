a25c4b51e53e1494f08d223297efa418ac3ff648
Sorokovsky <Sorokovskys@ukr.net>
2026-02-06 18:56:39 +0200

    feature: no-chats

--- src/components/page/chats-page/chats-page.ts
+++ src/components/page/chats-page/chats-page.ts
@@ -1,22 +1,16 @@
-import {Component, computed, type Signal} from '@angular/core';
-import {type GetChatsByMe, injectGetChatsByMe} from '@/injections/get-chats-by-me.query';
-import {injectRouteParameter} from '@/injections/route-parameter.injection';
+import {Component, type Signal} from '@angular/core';
 import {type Chat} from '@/contracts/chat.contract';
+import {injectCurrentChat} from '@/injections/current-chat.injection';
+import {NoChat} from '@/components/common/no-chat/no-chat';
 
 @Component({
   selector: 'app-chats-page',
-  imports: [],
+  imports: [
+    NoChat
+  ],
   templateUrl: './chats-page.html',
   styleUrl: './chats-page.sass',
 })
 export class ChatsPage {
-  protected readonly chatId: Signal<string | null> = injectRouteParameter("chatId");
-  private readonly chatsQuery: GetChatsByMe = injectGetChatsByMe();
-  protected currentChat: Signal<Chat | null> = computed((): Chat | null => {
-    const id: string | null = this.chatId();
-    if (id === null) return null;
-    if (Number.isNaN(id)) return null;
-    const chat: Chat[] = this.chatsQuery.data() || [];
-    return chat.find((item: Chat): boolean => item.id === Number(id)) ?? null;
-  });
+  protected currentChat: Signal<Chat | null> = injectCurrentChat();
 }
