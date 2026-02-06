7f347755a103c4117042512fed6c664e978f822c
Sorokovsky <Sorokovskys@ukr.net>
2026-02-06 18:56:56 +0200

    feature: no-chats

---
+++ src/components/common/no-chat/no-chat.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-no-chat',
+  imports: [],
+  templateUrl: './no-chat.html',
+  styleUrl: './no-chat.sass',
+})
+export class NoChat {
+
+}
