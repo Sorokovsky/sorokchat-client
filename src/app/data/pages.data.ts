82677561c4e5d52a3f57448fc2258702429613b9
Sorokovsky <Sorokovskys@ukr.net>
2026-02-10 21:58:57 +0200

    refactor: add segment for data in app

--- src/app/data/pages.data.ts
+++ src/app/data/pages.data.ts
@@ -1,5 +1,5 @@
-import {REGISTER_PAGE} from "@/pages";
-import type {Page} from "@/shared";
+import { REGISTER_PAGE } from '@/pages';
+import type { Page } from '@/shared';
 
 export const ANONYMOUS_PAGES: Page[] = [REGISTER_PAGE];
 export const ALL_PAGES: Page[] = Array.from(new Set([...ANONYMOUS_PAGES]));
