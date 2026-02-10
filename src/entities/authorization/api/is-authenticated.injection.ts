3be0497aa305b6786ccea27f305d52be423fcd7a
Sorokovsky <Sorokovskys@ukr.net>
2026-02-10 21:13:20 +0200

    feature: is authenticated

--- src/entities/authorization/api/is-authenticated.injection.ts
+++ src/entities/authorization/api/is-authenticated.injection.ts
@@ -1,8 +1,8 @@
-import type {Signal} from "@angular/core";
-import {computed} from "@angular/core";
+import type { Signal } from '@angular/core';
+import { computed } from '@angular/core';
 
-import type {GetProfileQuery} from "../models";
-import {injectGetProfile} from "./get-profile.injection";
+import type { GetProfileQuery } from '../models';
+import { injectGetProfile } from './get-profile.injection';
 
 export function injectIsAuthenticated(): Signal<boolean> {
   const profile: GetProfileQuery = injectGetProfile();
