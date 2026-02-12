bb07f7b8f4e07cdb43ddf577e33bfac2323ea181
Sorokovsky <Sorokovskys@ukr.net>
2026-02-12 16:39:45 +0200

    refactor: shared public api

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,6 +1,4 @@
 export { Button } from './button/button';
-export { FieldError } from './field-error/field-error';
 export { Form } from './form/form';
 export { UIInput } from './input/ui-input';
-export { Label } from './label/label';
 export { Loader } from './loader/loader';
