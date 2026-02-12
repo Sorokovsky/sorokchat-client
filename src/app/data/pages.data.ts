e5b4101f6ba8f01fcb0a34717e4c0ba9f1f47169
Sorokovsky <Sorokovskys@ukr.net>
2026-02-12 19:46:25 +0200

    fix: redirection

--- src/app/data/pages.data.ts
+++ src/app/data/pages.data.ts
@@ -1,11 +1,11 @@
-import { CHATS_PAGE, REGISTER_PAGE } from '@/pages';
-import { LOGIN_PAGE } from '@/pages/login/data/page.data';
-import type { Page } from '@/shared';
-import { removeDynamicPath } from '@/shared';
+import {CHATS_PAGE, REGISTER_PAGE} from "@/pages";
+import {LOGIN_PAGE} from "@/pages/login/data/page.data";
+import type {Page} from "@/shared";
+import {removeDynamicPath} from "@/shared";
 
 export const ANONYMOUS_PAGES: Page[] = [REGISTER_PAGE, LOGIN_PAGE];
 export const SECURED_PAGES: Page[] = [
   CHATS_PAGE,
   { ...CHATS_PAGE, path: removeDynamicPath(CHATS_PAGE.path) },
 ];
-export const ALL_PAGES: Page[] = Array.from(new Set([...ANONYMOUS_PAGES, ...SECURED_PAGES]));
+export const ALL_PAGES: Page[] = Array.from(new Set([...SECURED_PAGES, ...ANONYMOUS_PAGES]));
