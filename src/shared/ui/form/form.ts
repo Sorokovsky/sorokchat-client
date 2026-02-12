e1e2691d0602e65e569c48a7911d38bf35cf6b54
Sorokovsky <Sorokovskys@ukr.net>
2026-02-12 11:53:16 +0200

    feature: formGroup from fields

--- src/shared/ui/form/form.ts
+++ src/shared/ui/form/form.ts
@@ -1,16 +1,30 @@
-import type { InputSignal } from '@angular/core';
-import { Component, input } from '@angular/core';
+import type { InputSignal, Signal } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
+import type { FormGroup } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 import type { Field } from '../../models';
 
 @Component({
   selector: 'app-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form.html',
   styleUrl: './form.scss',
 })
 export class Form {
+  private readonly builder: FormBuilder = inject(FormBuilder);
   public readonly fields: InputSignal<Field[]> = input.required<Field[]>();
   public readonly title: InputSignal<string> = input.required<string>();
   public readonly submitText: InputSignal<string> = input.required<string>();
+  public form: Signal<FormGroup> = computed((): FormGroup => {
+    return this.builder.group(this.collectInputs(this.fields()));
+  });
+
+  private collectInputs(fields: Field[]): Record<string, string[]> {
+    const result: Record<string, string[]> = {};
+    for (const field of fields) {
+      result[field.name] = [field.defaultValue || ''];
+    }
+    return result;
+  }
 }
