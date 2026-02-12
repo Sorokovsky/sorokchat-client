a78f53f03ff3055855e41c88750b88641566ddcf
Sorokovsky <Sorokovskys@ukr.net>
2026-02-12 12:58:48 +0200

    feature: add data to register form

--- src/features/register/data/register.data.ts
+++ src/features/register/data/register.data.ts
@@ -1,4 +1,4 @@
-import type { Field } from '@/shared';
+import type {Field} from "@/shared";
 
 export const REGISTER_FIELDS: Field[] = [
   {
@@ -14,5 +14,26 @@ export const REGISTER_FIELDS: Field[] = [
     autocomplete: 'new-password',
     placeholder: 'Ваш пароль',
     label: 'Пароль',
+  },
+  {
+    type: 'text',
+    name: 'firstName',
+    autocomplete: 'name',
+    placeholder: "Ваше ім'я",
+    label: "Ім'я",
+  },
+  {
+    type: 'text',
+    name: 'lastName',
+    autocomplete: 'family-name',
+    placeholder: 'Ваше прізвище',
+    label: 'Прізвище',
+  },
+  {
+    type: 'text',
+    name: 'middleName',
+    autocomplete: 'additional-name',
+    placeholder: 'Ваше по батькові',
+    label: 'По батькові',
   },
 ];
