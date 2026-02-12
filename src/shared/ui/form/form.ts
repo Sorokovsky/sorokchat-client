87e73ed3599905fcc8293ef0201d0410ece1c3df
Sorokovsky <Sorokovskys@ukr.net>
2026-02-12 12:29:21 +0200

    feature: submit event

--- src/shared/ui/form/form.ts
+++ src/shared/ui/form/form.ts
@@ -1,5 +1,5 @@
-import type { InputSignal, Signal } from '@angular/core';
-import { Component, computed, inject, input } from '@angular/core';
+import type { InputSignal, OutputEmitterRef, Signal } from '@angular/core';
+import { Component, computed, inject, input, output } from '@angular/core';
 import type { FormGroup } from '@angular/forms';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import type { z as zod } from 'zod';
@@ -20,11 +20,18 @@ export class Form {
   public readonly title: InputSignal<string> = input.required<string>();
   public readonly submitText: InputSignal<string> = input.required<string>();
   public readonly schema: InputSignal<zod.Schema> = input.required<zod.Schema>();
+  public readonly send: OutputEmitterRef<unknown> = output<unknown>();
   public form: Signal<FormGroup> = computed((): FormGroup => {
     return this.builder.group(this.collectInputs(this.fields()), {
       validators: [zodValidation(this.schema())],
     });
   });
+
+  public onSubmit(): void {
+    const form: FormGroup = this.form();
+    if (form.valid) this.send.emit(form.value as unknown);
+    else form.markAllAsTouched();
+  }
 
   private collectInputs(fields: Field[]): Record<string, string[]> {
     const result: Record<string, string[]> = {};
