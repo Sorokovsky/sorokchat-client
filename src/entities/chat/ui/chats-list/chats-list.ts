a60b216fe72695fa64f1183538b1094bad655ba6
Sorokovsky <Sorokovskys@ukr.net>
2026-02-13 21:56:26 +0200

    feature: chats card component

--- src/entities/chat/ui/chats-list/chats-list.ts
+++ src/entities/chat/ui/chats-list/chats-list.ts
@@ -1,28 +1,16 @@
 import type { InputSignal } from '@angular/core';
 import { Component, input } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
 
-import { cutText } from '@/shared';
-
-import { MAX_DESCRIPTION_LENGTH } from '../../config';
 import type { Chat } from '../../models';
-import { ChatAvatar } from '../chat-avatar/chat-avatar';
+import { ChatCard } from '../chat-card/chat-card';
 
 @Component({
   selector: 'app-chats-list',
-  imports: [RouterLink, ChatAvatar, RouterLinkActive],
+  imports: [ChatCard],
   templateUrl: './chats-list.html',
   styleUrl: './chats-list.scss',
 })
 export class ChatsList {
   public readonly rootPath: InputSignal<string> = input.required<string>();
   public chats: InputSignal<Chat[]> = input.required<Chat[]>();
-
-  public getChatPath(chat: Chat): string {
-    return `${this.rootPath()}/${chat.id}`;
-  }
-
-  public getDescription(chat: Chat): string {
-    return cutText(chat.description, MAX_DESCRIPTION_LENGTH);
-  }
 }
