f9456ba5b9a232dd4a467fa194c28493701ab5f7
Sorokovsky <Sorokovskys@ukr.net>
2026-02-14 22:45:15 +0200

    messages service

--- src/shared/api/websocket.service.ts
+++ src/shared/api/websocket.service.ts
@@ -1,39 +1,39 @@
-import type { Signal, WritableSignal } from "@angular/core";
-import { inject, Injectable, Injector, signal } from "@angular/core";
-import { toSignal } from "@angular/core/rxjs-interop";
-import type { Frame, IMessage } from "@stomp/stompjs";
-import { Client } from "@stomp/stompjs";
-import type { Observer } from "rxjs";
-import { Observable } from "rxjs";
-import SockJS from "sockjs-client";
+import type { Signal, WritableSignal } from '@angular/core';
+import { inject, Injectable, Injector, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import type { Frame, IMessage } from '@stomp/stompjs';
+import { Client } from '@stomp/stompjs';
+import type { Observer } from 'rxjs';
+import { Observable } from 'rxjs';
+import SockJS from 'sockjs-client';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root',
 })
 export class WebSocketService {
   private readonly _isConnected: WritableSignal<boolean> = signal<boolean>(false);
   private readonly injector: Injector = inject(Injector);
-  private readonly messageQueue: { destination: string, body: string }[] = [];
+  private readonly messageQueue: { destination: string; body: string }[] = [];
   private subcribers: number = 0;
 
   private readonly client: Client;
 
   constructor() {
     this.client = new Client({
-      webSocketFactory: () => new SockJS("http://localhost:8080/ws"),
+      webSocketFactory: () => new SockJS('http://localhost:8080/ws'),
       reconnectDelay: 1000,
       heartbeatIncoming: 1000,
       heartbeatOutgoing: 1000,
-      debug: (str: string) => console.log(str)
+      debug: (str: string) => console.log(str),
     });
     this.setupHandlers(this.client);
   }
 
   public readonly isConnected: Signal<boolean> = this._isConnected.asReadonly();
 
-  public subscribe<T>(topic: string, initialValue: T | null = null): Signal<T | null> {
+  public subscribe<T>(topic: string): Signal<T | undefined> {
     this.connect();
-    const observable: Observable<T> = new Observable<T>((observer: Observer<T>): () => void => {
+    const observable: Observable<T> = new Observable<T>((observer: Observer<T>): (() => void) => {
       this.subcribers += 1;
       const subsrciption = this.client.subscribe(topic, (message: IMessage): void => {
         observer.next(JSON.parse(message.body) as T);
@@ -44,9 +44,9 @@ export class WebSocketService {
         setTimeout(() => {
           if (this.subcribers === 0) this.disconnect();
         }, 500);
-      }
+      };
     });
-    return toSignal(observable, { initialValue: initialValue as T, injector: this.injector });
+    return toSignal(observable, { initialValue: undefined, injector: this.injector });
   }
 
   public send<T>(destination: string, body: T): void {
@@ -74,7 +74,7 @@ export class WebSocketService {
   private setupHandlers(client: Client): void {
     client.onConnect = () => {
       this._isConnected.set(true);
-      console.log("STOMP Підключено");
+      console.log('STOMP Підключено');
       while (this.messageQueue.length > 0) {
         const message = this.messageQueue.shift();
         if (message) this.client.publish(message);
@@ -83,7 +83,7 @@ export class WebSocketService {
 
     client.onStompError = (frame: Frame): void => {
       this._isConnected.set(false);
-      console.log("STOMP помилка: ", frame);
+      console.log('STOMP помилка: ', frame);
     };
 
     client.onWebSocketClose = () => {
@@ -91,4 +91,4 @@ export class WebSocketService {
       console.log("WebSocket з'єднання закрито");
     };
   }
-};
+}
