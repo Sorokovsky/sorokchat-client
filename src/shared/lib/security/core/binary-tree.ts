83fcfd9ada12b1721941a14dcf0c2686b42f7a12
Sorokovsky <Sorokovskys@ukr.net>
2026-02-19 13:56:01 +0200

    feature: binary tree

--- src/shared/lib/security/core/binary-tree.ts
+++ src/shared/lib/security/core/binary-tree.ts
@@ -10,22 +10,24 @@ export class BinaryTree<T> {
     return BinaryTree.deepClone(this.rootNode);
   }
 
-  private static buildTree<T>(items: T[]): NodeTree<T> {
-    if (items.length === 0) return { current: null, right: null, left: null };
-    if (items.length === 1) return { current: items[0]!, left: null, right: null };
+  private static buildTree<T>(items: T[], parent: NodeTree<T> | null = null): NodeTree<T> {
+    if (items.length === 0) return { current: null, right: null, left: null, parent };
+    if (items.length === 1) return { current: items[0]!, left: null, right: null, parent };
     const middle: number = Math.floor(items.length / 2);
     const leftItems: T[] = items.slice(0, middle);
     const rightItems: T[] = items.slice(middle);
-    const left: NodeTree<T> | null = this.buildTree(leftItems);
-    const right: NodeTree<T> | null = this.buildTree(rightItems);
-    return { current: null, left, right };
+    const node: NodeTree<T> = { current: null, left: null, right: null, parent };
+    node.left = this.buildTree(leftItems, node);
+    node.right = this.buildTree(rightItems, node);
+    return node;
   }
 
-  private static deepClone<T>({ current, left, right }: NodeTree<T>): NodeTree<T> {
+  private static deepClone<T>({ current, left, right, parent }: NodeTree<T>): NodeTree<T> {
     return {
       current: current ? { ...current } : null,
       left: left ? this.deepClone(left) : null,
       right: right ? this.deepClone(right) : null,
+      parent: parent,
     };
   }
 }
--- src/shared/lib/security/core/node-tree.ts
+++ src/shared/lib/security/core/node-tree.ts
@@ -2,4 +2,5 @@ export interface NodeTree<T> {
   current: T | null;
   left: NodeTree<T> | null;
   right: NodeTree<T> | null;
+  parent: NodeTree<T> | null;
 }
