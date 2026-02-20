624433cd421d1b10c6d00e1b24107780b6e8b482
Sorokovsky <Sorokovskys@ukr.net>
2026-02-20 18:52:15 +0200

    fix: toggling sidebar

--- src/features/toggle-sidebar/ui/toggle-sidebar/toggle-sidebar.ts
+++ src/features/toggle-sidebar/ui/toggle-sidebar/toggle-sidebar.ts
@@ -1,4 +1,4 @@
-import type { ModelSignal } from '@angular/core';
+import type { ModelSignal, OnDestroy, OnInit } from '@angular/core';
 import { Component, model } from '@angular/core';
 import { MenuIcon } from 'lucide-angular';
 
@@ -11,12 +11,36 @@ import { ActionButton } from '@/shared';
   templateUrl: './toggle-sidebar.html',
   styleUrl: './toggle-sidebar.scss',
 })
-export class ToggleSidebar {
+export class ToggleSidebar implements OnInit, OnDestroy {
+  private static COUNT: number = 0;
+
   public isActive: ModelSignal<boolean> = model.required<boolean>();
+
+  public ngOnInit(): void {
+    ToggleSidebar.COUNT++;
+    if (ToggleSidebar.COUNT > 0) {
+      document.addEventListener('click', this.closeIfNotIcon.bind(this));
+    }
+  }
+
+  public ngOnDestroy(): void {
+    ToggleSidebar.COUNT--;
+    if (ToggleSidebar.COUNT < 1) {
+      document.removeEventListener('click', this.closeIfNotIcon.bind(this));
+    }
+  }
 
   protected icon: IconType = MenuIcon;
 
   protected toggle(): void {
     this.isActive.update((previous: boolean): boolean => !previous);
+  }
+
+  private closeIfNotIcon(event: PointerEvent): void {
+    const { target } = event;
+    if (!(target instanceof HTMLElement)) return;
+    const isToggler: boolean = target.closest('.toggle-button') === null;
+    const isLeftSidebar: boolean = target.closest('.left-sidebar') === null;
+    if (isToggler && isLeftSidebar) this.isActive.set(false);
   }
 }
