406b7aeac0116ef3d0e56970a9eb4791057715a7
Sorokovsky <Sorokovskys@ukr.net>
2026-02-26 18:43:35 +0200

    feature: form control

--- src/shared/ui/form/abstract-form.ts
+++ src/shared/ui/form/abstract-form.ts
@@ -1,11 +1,14 @@
-import type { Signal } from '@angular/core';
-import { computed, inject } from '@angular/core';
+import type { OnInit, Signal, WritableSignal } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import type { FormControl, FormGroup } from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
 import type { GenericSchema } from 'valibot';
 
 import type { Field } from '../../models';
 
+@Component({
+  template: '',
+})
 export abstract class AbstractFormComponent<T> {
   private readonly formBuilder: FormBuilder = inject(FormBuilder);
 
@@ -23,11 +26,15 @@ export abstract class AbstractFormComponent<T> {
     return this.formGroup().get(name) as FormControl;
   }
 
-  private collectControls(fields: Field[]) {
+  private collectControls(fields: Field[]): Record<string, FormControl> {
     const result: Record<string, FormControl> = {};
     for (const { defaultValue, name } of fields) {
-      result[name] = this.formBuilder.control(defaultValue ? defaultValue : '');
+      result[name] = this.formBuilder.control(defaultValue || '');
     }
     return result;
+  }
+
+  protected onSubmit(): void {
+    const form: FormGroup = this.formGroup();
   }
 }
